782f1fe195ae4a09cf1648809a32ef5152856595
J0suu <josuherdroid@gmail.com>
2022-09-13 19:02:40 +0100

    Update exercise.ts
    add refactor

--- src/E07_pokemon_battle/exercise.ts
+++ src/E07_pokemon_battle/exercise.ts
@@ -13,30 +13,32 @@ export function getAttackDamage(
 	attack: number,
 	defense: number
 ): number {
-	let efectivity = 0;
-
 	if (attack <= 0 || defense <= 0 || attack > 100 || defense > 100) {
 		return -1;
 	}
 
-	if (
-		(attackerType == 'water' && defenderType == 'fire') ||
-		(attackerType == 'fire' && defenderType == 'grass') ||
-		(attackerType == 'grass' && defenderType == 'water') ||
-		(attackerType == 'electric' && defenderType == 'water')
-	) {
-		efectivity = 2;
-	} else if (
-		(attackerType == 'water' && defenderType == 'electric') ||
-		(attackerType == 'fire' && defenderType == 'electric') ||
-		(attackerType == 'grass' && defenderType == 'electric') ||
-		(attackerType == 'electric' && defenderType == 'fire')
-	) {
-		efectivity = 1;
-	} else {
-		efectivity = 0.5;
-	}
+	const efectivity = getEffectivity(attackerType, defenderType);
 
 	let damage = 50 * (attack / defense) * efectivity;
 	return parseFloat(damage.toFixed(2));
+}
+function getEffectivity(attackerType: string, defenderType: string) {
+	const isVeryEffective =
+		(attackerType == 'water' && defenderType == 'fire') ||
+		(attackerType == 'fire' && defenderType == 'grass') ||
+		(attackerType == 'grass' && defenderType == 'water') ||
+		(attackerType == 'electric' && defenderType == 'water');
+
+	const isNormalEffective =
+		(attackerType == 'water' && defenderType == 'electric') ||
+		(attackerType == 'fire' && defenderType == 'electric') ||
+		(attackerType == 'grass' && defenderType == 'electric') ||
+		(attackerType == 'electric' && defenderType == 'fire');
+
+	if (isVeryEffective) {
+		return 2;
+	} else if (isNormalEffective) {
+		return 1;
+	}
+	return 0.5;
 }
