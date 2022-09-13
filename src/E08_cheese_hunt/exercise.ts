02baedfb8f3bc82d76cb9bfb6869928281bce42b
J0suu <josuherdroid@gmail.com>
2022-09-13 20:47:34 +0100

    get mouse position from exercise 8

--- src/E08_cheese_hunt/exercise.ts
+++ src/E08_cheese_hunt/exercise.ts
@@ -7,7 +7,21 @@ enum DirectionEnum {
 
 type DirectionType = keyof typeof DirectionEnum;
 
+let getMousePosition = (room: string[][]): { x: number; y: number } | null => {
+	for (let i = 0; i < room.length; i++) {
+		for (let j = 0; j < room[i].length; j++) {
+			if (room[i][j] === 'm') {
+				return { x: i, y: j };
+			}
+		}
+	}
+	return null;
+};
+
 export function canMouseEat(direction: DirectionType, game: string[][]): boolean {
-	// Escribe tu código aquí
-	return false;
+	let mousePosition = getMousePosition(game);
+	if (!mousePosition) {
+		return false;
+	}
+	return true;
 }
